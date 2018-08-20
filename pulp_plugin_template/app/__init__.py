6ca43369d7b4efcdaca555836fa5316fab4d9859
Daniel Alley <dalley@redhat.com>
2018-08-20 18:50:44 -0400

    Add docstring

--- pulp_plugin_template/app/__init__.py
+++ pulp_plugin_template/app/__init__.py
@@ -2,5 +2,6 @@ from pulpcore.plugin import PulpPluginAppConfig
 
 
 class PulpPluginTemplatePluginAppConfig(PulpPluginAppConfig):
+    """ Entry point for the plugin_template plugin."""
     name = 'pulp_plugin_template.app'
     label = 'pulp_plugin_template'
