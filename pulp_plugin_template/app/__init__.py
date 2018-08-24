8bd6ef205e4c97889ab1a1eff500cc82cab58ae5
Daniel Alley <dalley@redhat.com>
2018-08-24 15:07:11 -0400

    Fix a formatting warning

--- pulp_plugin_template/app/__init__.py
+++ pulp_plugin_template/app/__init__.py
@@ -2,6 +2,7 @@ from pulpcore.plugin import PulpPluginAppConfig
 
 
 class PulpPluginTemplatePluginAppConfig(PulpPluginAppConfig):
-    """ Entry point for the plugin_template plugin."""
+    """Entry point for the plugin_template plugin."""
+
     name = 'pulp_plugin_template.app'
     label = 'pulp_plugin_template'
