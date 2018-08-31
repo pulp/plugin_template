f03e727a2186f9446853646272a752b8ca01210b
Daniel Alley <dalley@redhat.com>
2018-08-30 20:59:48 -0400

    Add new test for sync with invalid Remote URL

--- pulp_plugin_template/tests/functional/api/test_sync.py
+++ pulp_plugin_template/tests/functional/api/test_sync.py
@@ -2,7 +2,7 @@
 """Tests that sync plugin_template plugin repositories."""
 import unittest
 
-from pulp_smash import api, config
+from pulp_smash import api, config, exceptions
 from pulp_smash.pulp3.constants import REPO_PATH
 from pulp_smash.pulp3.utils import (
     gen_repo,
@@ -21,13 +21,14 @@ from pulp_plugin_template.tests.functional.utils import set_up_module as setUpMo
 
 # Implement sync support before enabling this test.
 @unittest.skip("FIXME: plugin writer action required")
-class BasicSyncPluginTemplateRepoTestCase(unittest.TestCase):
+class BasicSyncTestCase(unittest.TestCase):
     """Sync repositories with the plugin_template plugin."""
 
     @classmethod
     def setUpClass(cls):
         """Create class-wide variables."""
         cls.cfg = config.get_config()
+        cls.client = api.Client(cls.cfg, api.json_handler)
 
     def test_sync(self):
         """Sync repositories with the plugin_template plugin.
@@ -45,19 +46,17 @@ class BasicSyncPluginTemplateRepoTestCase(unittest.TestCase):
         5. Sync the remote one more time.
         6. Assert that repository version is different from the previous one.
         """
-        client = api.Client(self.cfg, api.json_handler)
-
-        repo = client.post(REPO_PATH, gen_repo())
-        self.addCleanup(client.delete, repo['_href'])
+        repo = self.client.post(REPO_PATH, gen_repo())
+        self.addCleanup(self.client.delete, repo['_href'])
 
         body = gen_plugin_template_remote()
-        remote = client.post(PLUGIN_TEMPLATE_REMOTE_PATH, body)
-        self.addCleanup(client.delete, remote['_href'])
+        remote = self.client.post(PLUGIN_TEMPLATE_REMOTE_PATH, body)
+        self.addCleanup(self.client.delete, remote['_href'])
 
         # Sync the repository.
         self.assertIsNone(repo['_latest_version_href'])
         sync(self.cfg, remote, repo)
-        repo = client.get(repo['_href'])
+        repo = self.client.get(repo['_href'])
 
         self.assertIsNotNone(repo['_latest_version_href'])
         self.assertEqual(len(get_content(repo)), PLUGIN_TEMPLATE_FIXTURE_COUNT)
@@ -66,8 +65,32 @@ class BasicSyncPluginTemplateRepoTestCase(unittest.TestCase):
         # Sync the repository again.
         latest_version_href = repo['_latest_version_href']
         sync(self.cfg, remote, repo)
-        repo = client.get(repo['_href'])
+        repo = self.client.get(repo['_href'])
 
         self.assertNotEqual(latest_version_href, repo['_latest_version_href'])
         self.assertEqual(len(get_content(repo)), PLUGIN_TEMPLATE_FIXTURE_COUNT)
         self.assertEqual(len(get_added_content(repo)), 0)
+
+
+class SyncInvalidURLTestCase(unittest.TestCase):
+    """Sync a repository with an invalid url on the Remote."""
+
+    def test_all(self):
+        """
+        Sync a repository using a Remote url that does not exist.
+
+        Test that we get a task failure.
+
+        """
+        cfg = config.get_config()
+        client = api.Client(cfg, api.json_handler)
+
+        repo = client.post(REPO_PATH, gen_repo())
+        self.addCleanup(client.delete, repo['_href'])
+
+        body = gen_plugin_template_remote(url="http://i-am-an-invalid-url.com/invalid/")
+        remote = client.post(PLUGIN_TEMPLATE_REMOTE_PATH, body)
+        self.addCleanup(client.delete, remote['_href'])
+
+        with self.assertRaises(exceptions.TaskReportError):
+            sync(cfg, remote, repo)
