52fc2548c1f18b2e30225c2c0f409df707278236
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-03-05 16:17:20 -0300

    Introducing tests against minio
    Required PR: https://github.com/pulp/pulpcore/pull/568
    Required PR: https://github.com/pulp/pulp-operator/pull/35
    https://pulp.plan.io/issues/5962
    closes #5962

--- .travis/pre_before_install.sh
+++ .travis/pre_before_install.sh
@@ -5,6 +5,7 @@ pip install -r test_requirements.txt
 mkdir ../pulp_catdog/.travis
 touch ../pulp_catdog/.travis/test_bindings.py
 echo 'pypi_username: the_pypi_user' >> ../pulp_catdog/template_config.yml
+sed -i "s/test_s3: false/test_s3: true/g" ../pulp_catdog/template_config.yml
 ./plugin-template --all pulp_catdog
 mv ../pulp_catdog/*.txt .
 mv ../pulp_catdog/.travis/before_install.sh ../pulp_catdog/.travis/old_before_install.sh
