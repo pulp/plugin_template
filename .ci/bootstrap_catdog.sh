392daaeac3a6bc389e656811080f09c759b83d50
Matthias Dellweg <mdellweg@redhat.com>
2020-11-20 15:30:49 +0100

    Test plugin bootstrapping
    fixes #7860
    https://pulp.plan.io/issues/7860

--- .ci/bootstrap_catdog.sh
+++ .ci/bootstrap_catdog.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -euv
+
 pip install -r test_requirements.txt
 ./plugin-template --generate-config --plugin-app-label catdog pulp_catdog
-mkdir ../pulp_catdog/.travis
-touch ../pulp_catdog/.travis/test_bindings.rb
+mkdir -p ../pulp_catdog/.github/workflows/scripts
+touch ../pulp_catdog/.github/workflows/scripts/test_bindings.py
+touch ../pulp_catdog/.github/workflows/scripts/test_bindings.rb
 echo 'pypi_username: the_pypi_user' >> ../pulp_catdog/template_config.yml
 sed -i "s/test_s3: false/test_s3: true/g" ../pulp_catdog/template_config.yml
 ./plugin-template --all pulp_catdog
@@ -11,13 +14,15 @@ sed -i "s/test_s3: false/test_s3: true/g" ../pulp_catdog/template_config.yml
 cd ../pulp_catdog
 
 # ignore unused imports
-flake8 --config flake8.cfg .travis || exit 1 # check travis files before ignoring imports
+flake8 --config flake8.cfg .ci || exit 1 # check travis files before ignoring imports
 sed -i -e '/^ignore/s/$/,F401/' flake8.cfg
 
 # include post_before_script to generate migrations
-cp ../plugin_template/.travis/post_before_script.sh .travis/
+cp ../plugin_template/.ci/post_before_script.sh .github/workflows/scripts
 
 # generate a git commit
 git init
+git config user.name "Cat Dog"
+git config user.email "pulp@cat.dog"
 git add .
 git commit -m $'Initial commit\n\n[noissue]'
---
+++ CHANGES/7860.feature
@@ -0,0 +1,1 @@
+Added a workflow to test the bootstrapping mechanism for a new plugin
