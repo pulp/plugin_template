b9c7ba732aff514a1759c5657a52d465939eb956
Matthias Dellweg <mdellweg@redhat.com>
2024-07-01 16:20:48 +0200

    Stop running docs tests on release branches

--- scripts/update_ci.sh
+++ scripts/update_ci.sh
@@ -2,9 +2,20 @@
 
 set -eu
 
-if [ ! -f "template_config.yml" ]; then
+if [ ! -f "template_config.yml" ]
+then
   echo "No template_config.yml detected."
   exit 1
+fi
+
+if [ "${1:-}" = "--release" ]
+then
+  echo "Running on a release branch"
+
+  sed -i \
+    -e 's/^\(ci_update_docs: \)true$/\1false/' \
+    -e 's/^\(docs_test: \)true$/\1false/' \
+    "template_config.yml"
 fi
 
 plugin_name="$(python ../plugin_template/scripts/get_template_config_value.py plugin_name)"
@@ -12,19 +23,19 @@ ci_update_docs="$(python ../plugin_template/scripts/get_template_config_value.py
 noissue_marker="$(python ../plugin_template/scripts/get_template_config_value.py noissue_marker)"
 use_black="$(python ../plugin_template/scripts/get_template_config_value.py black)"
 
-if [[ -z "$noissue_marker" ]]; then
+if [[ -z "${noissue_marker}" ]]; then
   noissue_marker="[noissue]"
 fi
 
-if [[ "$ci_update_docs" == "True" ]]; then
-  docs="--docs"
+if [[ "${ci_update_docs}" == "True" ]]; then
+  docs=("--docs")
 else
-  docs=""
+  docs=()
 fi
 
 pushd ../plugin_template
 pip install -r requirements.txt
-./plugin-template --github $docs $plugin_name
+./plugin-template --github "${docs[@]}" "${plugin_name}"
 popd
 
 # Check if only gitref file has changed, so no effect on CI workflows.
@@ -35,19 +46,20 @@ fi
 
 if [[ $(git status --porcelain) ]]; then
   git add -A
-  git commit -m "Update CI files" -m "$noissue_marker"
+  git commit -m "Update CI files" -m "${noissue_marker}"
 else
   echo "No updates needed"
 fi
 
-if [[ "$use_black" = "True" ]]
+if [[ "${use_black}" == "True" ]]
 then
   pip install -r lint_requirements.txt
   black .
 
-  if [[ $(git status --porcelain) ]]; then
+  if [[ "$(git status --porcelain)" ]]
+  then
     git add -A
-    git commit -m "Reformat with black" -m "$noissue_marker"
+    git commit -m "Reformat with black" -m "${noissue_marker}"
   else
     echo "No formatting change needed"
   fi
